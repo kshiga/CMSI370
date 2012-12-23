0a3a4781096e87baa4ce19d5d95680f163946056
KSH <ksh@ubuntu.(none)>
2012-12-22 18:43:06 -0800

    Can get to calculate, but not quite to write to the box below.  Must also fix caluclation.

--- 1204/slider.js
+++ 1204/slider.js
@@ -3,23 +3,23 @@ $(function (){
 
 var report = function(a){
     $("#gpaReport").html(a);
-}
 
-var calculate = function() {
-    var kCurrent = $("#knob")
-    var position = kCurrent.position();
-    return ((position.left /200) *4);
 }
 
 
     $( "#knob" ).draggable({
         axis: "x", 
-        containment: "parent"
+        containment: "parent",
+        stop: function(evt, ui){          
+            var kCurrent = $("#knob")
+            var position = kCurrent.position();
+            console.log("Position: " + position.left);
+            var gpaCalc = ((position.left /200) *4);
+            console.log("gpaCalc: " + gpaCalc);
+            report(gpaCalc);
+        }
     });
     
-    $("#knob").mouseup(
-          calculate()
-     );
 
 })
 
